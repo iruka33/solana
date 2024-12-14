e964c889d83db630f45d905f06369b0afbb6e771
iruka33 <andrusksp@gmail.com>
2024-12-14 19:51:16 +0700

    Update macros.rs

--- measure/src/macros.rs
+++ measure/src/macros.rs
@@ -1,188 +1,148 @@
-/// Measure this expression
-///
-/// Use `measure!()` when you have an expression that you want to measure.  `measure!()` will start
-/// a new [`Measure`], evaluate your expression, stop the [`Measure`], and then return the
-/// [`Measure`] object along with your expression's return value.
-///
-/// Use `measure_us!()` when you want to measure an expression in microseconds.
-///
-/// [`Measure`]: crate::measure::Measure
-///
-/// # Examples
-///
-/// ```
-/// // Measure functions
-/// # use solana_measure::{measure, measure_us};
-/// # fn foo() {}
-/// # fn bar(x: i32) {}
-/// # fn add(x: i32, y: i32) -> i32 {x + y}
-/// let (result, measure) = measure!(foo(), "foo takes no parameters");
-/// let (result, measure) = measure!(bar(42), "bar takes one parameter");
-/// let (result, measure) = measure!(add(1, 2), "add takes two parameters and returns a value");
-/// let (result, measure_us) = measure_us!(add(1, 2));
-/// # assert_eq!(result, 1 + 2);
-/// ```
-///
-/// ```
-/// // Measure methods
-/// # use solana_measure::{measure, measure_us};
-/// # struct Foo {
-/// #     f: i32,
-/// # }
-/// # impl Foo {
-/// #     fn frobnicate(&self, bar: i32) -> i32 {
-/// #         self.f * bar
-/// #     }
-/// # }
-/// let foo = Foo { f: 42 };
-/// let (result, measure) = measure!(foo.frobnicate(2), "measure methods");
-/// let (result, measure_us) = measure_us!(foo.frobnicate(2));
-/// # assert_eq!(result, 42 * 2);
-/// ```
-///
-/// ```
-/// // Measure expression blocks
-/// # use solana_measure::measure;
-/// # fn complex_calculation() -> i32 { 42 }
-/// # fn complex_transform(x: i32) -> i32 { x + 3 }
-/// # fn record_result(y: i32) {}
-/// let (result, measure) = measure!(
-///     {
-///         let x = complex_calculation();
-///         # assert_eq!(x, 42);
-///         let y = complex_transform(x);
-///         # assert_eq!(y, 42 + 3);
-///         record_result(y);
-///         y
-///     },
-///     "measure a block of many operations",
-/// );
-/// # assert_eq!(result, 42 + 3);
-/// ```
-///
-/// ```
-/// // The `name` parameter is optional
-/// # use solana_measure::{measure, measure_us};
-/// # fn meow() {};
-/// let (result, measure) = measure!(meow());
-/// let (result, measure_us) = measure_us!(meow());
-/// ```
-#[macro_export]
-macro_rules! measure {
-    ($val:expr, $name:tt $(,)?) => {{
-        let mut measure = $crate::measure::Measure::start($name);
-        let result = $val;
-        measure.stop();
-        (result, measure)
-    }};
-    ($val:expr) => {
-        measure!($val, "")
-    };
+use {
+    solana_sdk::timing::{duration_as_ms, duration_as_ns, duration_as_s, duration_as_us},
+    std::{
+        fmt,
+        time::{Duration, Instant},
+    },
+};
+
+#[derive(Debug)]
+pub struct Measure {
+    name: &'static str,
+    start: Instant,
+    duration: u64,
 }
 
-#[macro_export]
-macro_rules! measure_us {
-    ($val:expr) => {{
-        let start = std::time::Instant::now();
-        let result = $val;
-        (result, solana_sdk::timing::duration_as_us(&start.elapsed()))
-    }};
+impl Measure {
+    pub fn start(name: &'static str) -> Self {
+        Self {
+            name,
+            start: Instant::now(),
+            duration: 0,
+        }
+    }
+
+    pub fn stop(&mut self) {
+        self.duration = duration_as_ns(&self.start.elapsed());
+    }
+
+    pub fn as_ns(&self) -> u64 {
+        self.duration
+    }
+
+    pub fn as_us(&self) -> u64 {
+        self.duration / 1000
+    }
+
+    pub fn as_ms(&self) -> u64 {
+        self.duration / (1000 * 1000)
+    }
+
+    pub fn as_s(&self) -> f32 {
+        self.duration as f32 / (1000.0f32 * 1000.0f32 * 1000.0f32)
+    }
+
+    pub fn as_duration(&self) -> Duration {
+        Duration::from_nanos(self.as_ns())
+    }
+
+    pub fn end_as_ns(self) -> u64 {
+        duration_as_ns(&self.start.elapsed())
+    }
+
+    pub fn end_as_us(self) -> u64 {
+        duration_as_us(&self.start.elapsed())
+    }
+
+    pub fn end_as_ms(self) -> u64 {
+        duration_as_ms(&self.start.elapsed())
+    }
+
+    pub fn end_as_s(self) -> f32 {
+        duration_as_s(&self.start.elapsed())
+    }
+
+    pub fn end_as_duration(self) -> Duration {
+        self.start.elapsed()
+    }
+}
+
+impl fmt::Display for Measure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.duration == 0 {
+            write!(f, "{} running", self.name)
+        } else if self.as_us() < 1 {
+            write!(f, "{} took {}ns", self.name, self.duration)
+        } else if self.as_ms() < 1 {
+            write!(f, "{} took {}us", self.name, self.as_us())
+        } else if self.as_s() < 1. {
+            write!(f, "{} took {}ms", self.name, self.as_ms())
+        } else {
+            write!(f, "{} took {:.1}s", self.name, self.as_s())
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{thread::sleep, time::Duration};
+    use {super::*, std::thread::sleep};
 
-    fn my_multiply(x: i32, y: i32) -> i32 {
-        x * y
-    }
-
-    fn square(x: i32) -> i32 {
-        my_multiply(x, x)
-    }
-
-    struct SomeStruct {
-        x: i32,
-    }
-    impl SomeStruct {
-        fn add_to(&self, x: i32) -> i32 {
-            x + self.x
-        }
+    #[test]
+    fn test_measure() {
+        let test_duration = Duration::from_millis(100);
+        let mut measure = Measure::start("test");
+        sleep(test_duration);
+        measure.stop();
+        assert!(measure.as_duration() >= test_duration);
     }
 
     #[test]
-    fn test_measure_macro() {
-        // Ensure that the measurement side actually works
-        {
-            let (_result, measure) = measure!(sleep(Duration::from_secs(1)), "test");
-            assert!(measure.as_s() >= 0.99f32 && measure.as_s() <= 1.01f32);
-            assert!(measure.as_ms() >= 990 && measure.as_ms() <= 1_010);
-            assert!(measure.as_us() >= 999_000 && measure.as_us() <= 1_010_000);
-        }
+    fn test_measure_as() {
+        let test_duration = Duration::from_millis(100);
+        let measure = Measure {
+            name: "test",
+            start: Instant::now(),
+            duration: test_duration.as_nanos() as u64,
+        };
 
-        // Ensure that the macro can be called with functions
-        {
-            let (result, _measure) = measure!(my_multiply(3, 4), "test");
-            assert_eq!(result, 3 * 4);
-
-            let (result, _measure) = measure!(square(5), "test");
-            assert_eq!(result, 5 * 5)
-        }
-
-        // Ensure that the macro can be called with methods
-        {
-            let some_struct = SomeStruct { x: 42 };
-            let (result, _measure) = measure!(some_struct.add_to(4), "test");
-            assert_eq!(result, 42 + 4);
-        }
-
-        // Ensure that the macro can be called with blocks
-        {
-            let (result, _measure) = measure!({ 1 + 2 }, "test");
-            assert_eq!(result, 3);
-        }
-
-        // Ensure that the macro can be called with a trailing comma
-        {
-            let (result, _measure) = measure!(square(5), "test",);
-            assert_eq!(result, 5 * 5)
-        }
-
-        // Ensure that the macro can be called without a name
-        {
-            let (result, _measure) = measure!(square(5));
-            assert_eq!(result, 5 * 5)
-        }
+        assert!(f32::abs(measure.as_s() - 0.1f32) <= f32::EPSILON);
+        assert_eq!(measure.as_ms(), 100);
+        assert_eq!(measure.as_us(), 100_000);
+        assert_eq!(measure.as_ns(), 100_000_000);
+        assert_eq!(measure.as_duration(), test_duration);
     }
 
     #[test]
-    fn test_measure_us_macro() {
-        // Ensure that the measurement side actually works
-        {
-            let (_result, measure) = measure_us!(sleep(Duration::from_secs(1)));
-            assert!((999_000..=1_010_000).contains(&measure));
-        }
+    fn test_measure_display() {
+        let measure = Measure {
+            name: "test_ns",
+            start: Instant::now(),
+            duration: 1,
+        };
+        assert_eq!(format!("{measure}"), "test_ns took 1ns");
 
-        // Ensure that the macro can be called with functions
-        {
-            let (result, _measure) = measure_us!(my_multiply(3, 4));
-            assert_eq!(result, 3 * 4);
+        let measure = Measure {
+            name: "test_us",
+            start: Instant::now(),
+            duration: 1000,
+        };
+        assert_eq!(format!("{measure}"), "test_us took 1us");
 
-            let (result, _measure) = measure_us!(square(5));
-            assert_eq!(result, 5 * 5)
-        }
+        let measure = Measure {
+            name: "test_ms",
+            start: Instant::now(),
+            duration: 1000 * 1000,
+        };
+        assert_eq!(format!("{measure}"), "test_ms took 1ms");
 
-        // Ensure that the macro can be called with methods
-        {
-            let some_struct = SomeStruct { x: 42 };
-            let (result, _measure) = measure_us!(some_struct.add_to(4));
-            assert_eq!(result, 42 + 4);
-        }
+        let measure = Measure {
+            name: "test_s",
+            start: Instant::now(),
+            duration: 1000 * 1000 * 1000,
+        };
+        assert_eq!(format!("{measure}"), "test_s took 1.0s");
 
-        // Ensure that the macro can be called with blocks
-        {
-            let (result, _measure) = measure_us!({ 1 + 2 });
-            assert_eq!(result, 3);
-        }
+        let measure = Measure::start("test_not_stopped");
+        assert_eq!(format!("{measure}"), "test_not_stopped running");
     }
 }
